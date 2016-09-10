24e76043116b85db8451c3b30fb5f7a92a0b840b
Rishi Srivastava <rishi@v1022-rn-101-218.campus-dynamic.uwaterloo.ca>
2016-09-10 00:20:21 -0400

    First commit

---
+++ iconanim.js
@@ -0,0 +1,46 @@
+
+function func1(status) {
+    if (status == "on"){
+        document.getElementById("id1").style.color = "#527cff";
+    }
+    else{
+        document.getElementById("id1").style.color = "#fff749";
+    }
+
+}
+function func2(status) {
+    if (status == "on"){
+        document.getElementById("id2").style.color = "#527cff";
+    }
+    else{
+        document.getElementById("id2").style.color = "#fff749";
+    }
+
+}
+function func3(status) {
+    if (status == "on"){
+        document.getElementById("id3").style.color = "#527cff";
+    }
+    else{
+        document.getElementById("id3").style.color = "#fff749";
+    }
+
+}
+function func4(status) {
+    if (status == "on"){
+        document.getElementById("id4").style.color = "#527cff";
+    }
+    else{
+        document.getElementById("id4").style.color = "#fff749";
+    }
+
+}
+function func5(status) {
+    if (status == "on"){
+        document.getElementById("id5").style.color = "#527cff";
+    }
+    else{
+        document.getElementById("id5").style.color = "#fff749";
+    }
+
+}
